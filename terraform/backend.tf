58250bc32fe3088c89200e74f5dcfd048ab02009
alkhawli <alkhawli@users.noreply.github.com>
2026-09-15 10:40:33 +0200

    Temporarily disable backend config until bootstrap is complete

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,11 +2,12 @@
 # This storage account should be created FIRST before migrating state
 # Run the bootstrap commands in the README before enabling this backend
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "pagent-tfstate-rg"
-    storage_account_name = "pagenttfstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
+# TEMPORARILY COMMENTED OUT - Will enable after bootstrapping the backend
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "pagent-tfstate-rg"
+#     storage_account_name = "pagenttfstate"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+# }
